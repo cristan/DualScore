e7fdb1a8042c120851ee48288d88c92b86e61f4e
Cristan Meijer <cristan.meijer@gmail.com>
2023-02-19 10:40:01 +0100

    Also add -5 and -10 buttons

--- src/jvmMain/kotlin/InputScreen.kt
+++ src/jvmMain/kotlin/InputScreen.kt
@@ -101,6 +101,26 @@ private fun TeamControls(
         Button(
             modifier = Modifier.padding(start = 20.dp),
             onClick = {
+                onTeamScoreUpdate(team.score - 10)
+            }) {
+            Text(
+                text = "-10",
+                style = MaterialTheme.typography.h5
+            )
+        }
+        Button(
+            modifier = Modifier.padding(start = 10.dp),
+            onClick = {
+                onTeamScoreUpdate(team.score - 5)
+            }) {
+            Text(
+                text = "-5",
+                style = MaterialTheme.typography.h5
+            )
+        }
+        Button(
+            modifier = Modifier.padding(start = 10.dp),
+            onClick = {
                 onTeamScoreUpdate(team.score - 1)
             }) {
             Text(
